fa7b107623bfb5cc52be8992f4ef4cb6f9703b85
psalmzoe <psalmzoe007@gmail.com>
2025-10-21 09:30:15 +0100

    updated landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+export const dynamic = "force-dynamic"
+
 import Link from "next/link"
 import { ArrowRight, Newspaper, Sparkles, Heart, TrendingUp, Zap, Shield } from "lucide-react"
 import { Button } from "@/components/ui/button"
